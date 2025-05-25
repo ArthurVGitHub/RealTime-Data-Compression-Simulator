56893a0f1ee3658009a004120317c8d1325a2676
ArthurVGitHub <arthur.valentin.zuun@gmail.com>
2025-05-25 20:53:12 +0100

    removed error

--- compare.cpp
+++ compare.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cmath>
-
-bool compareVectors(const std::vector<double>& a, const std::vector<double>& b, double epsilon = 1e-9) {
+#include "compare.h"
+bool compareVectors(const std::vector<double>& a, const std::vector<double>& b, double epsilon) {
     if (a.size() != b.size()) return false;
     for (size_t i = 0; i < a.size(); ++i) {
         if (std::abs(a[i] - b[i]) > epsilon) return false;
