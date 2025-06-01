0f1156ec665bb4d41d1338028e518ffb1e138191
ArthurVGitHub <arthur.valentin.zuun@gmail.com>
2025-06-01 13:32:14 +0100

    Removed comments

--- window_optimizer.cpp
+++ window_optimizer.cpp
@@ -28,21 +28,6 @@ int WindowOptimizer::updateWindowSize(
     return newSize;
 }
 
-/*int WindowOptimizer::updateWindowSize(const std::vector<double> &window, int currentSize, int minWindow, int maxWindow,
-                                      double lowVar, double highVar) {
-    if (window.empty()) return currentSize;
-    double mean = 0.0;
-    for (double v : window) mean += v;
-    mean /= window.size();
-    double var = 0.0;
-    for (double v : window) var += (v - mean) * (v - mean);
-    var /= window.size();
-
-    if (var < lowVar && currentSize < maxWindow) return currentSize + 1;
-    if (var > highVar && currentSize > minWindow) return currentSize - 1;
-    std::cout << "Window variance: " << var << " | Current size: " << currentSize << "\n";
-    return currentSize;
-}*/
 //UNDER TEST:
 /*
 int WindowOptimizer::optimizeWindowSizeForCR(const std::vector<double> &pastWindow, int currentSize, int minWindow,
